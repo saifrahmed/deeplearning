9e3a736728e782aaebcc61b51d0df0c2d2941112
sahmedkinetica <sahmed@kinetica.com>
2019-12-02 20:52:53 -0500

    Includes runner scripts

--- launch_cpu.sh
+++ launch_cpu.sh
@@ -3,3 +3,6 @@
 repo_uri_cpu=$(cat repo_uri.info.CPU)
 
 echo "Launching CPU learning container from $repo_uri_cpu using Docker Standard CPU Runtime"
+
+docker run -p 8888:8888 saifrahmed/deeplearning-cpu:r1
+# TODO: launch CLI with Jupyter in background
--- launch_gpu.sh
+++ launch_gpu.sh
@@ -3,3 +3,6 @@
 repo_uri_gpu=$(cat repo_uri.info.GPU)
 
 echo "Launching GPU learning container from $repo_uri_gpu using Docker NVIDIA Runtime"
+
+docker run -p 8888:8888 --runtime=nvidia saifrahmed/deeplearning-gpu:r1
+# TODO: launch CLI with Jupyter in background
